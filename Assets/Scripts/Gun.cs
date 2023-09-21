ee9f1a0e87ea0ae8ce59fbc0d696d03867d235d4
pswxxk <sangwook2001@gmail.com>
2023-09-21 13:57:05 +0900

    총 발사 구현
    좀비를 죽이는 101가지 방법중 한가지

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -32,20 +32,82 @@ public class Gun : MonoBehaviour {
 
     private void Awake() {
         // 사용할 컴포넌트의 참조 가져오기
+        gunAudioPlayer = GetComponent<AudioSource>(); 
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+        //사용할 점을 두개로 변경
+        bulletLineRenderer.positionCount = 2;
+        //라인 렌더러를 비활성화
+        bulletLineRenderer.enabled = false;
     }
 
     private void OnEnable() {
         // 총 상태 초기화
+
+        //전체 예비 탄알 양을 초기화
+        ammoRemain = gunData.startAmmoRemain;
+        //현재 탄창을 가득 채우기
+        magAmmo = gunData.magCapacity;
+
+        //총의 현재 상태를 총을 쏠 준비가 된 상태로 변경
+        state = State.Ready;
+        //마지막으로 총을 쏜 시점을 초기화
+        lastFireTime = 0;
     }
 
     // 발사 시도
     public void Fire() {
+        //현재 상태가 발사 가능한 상태
+        //&& 마지막 총 발사 시점에서 gunData.timeBetFire 이상의 시간이 지남
+        if(state == State.Ready && Time.time >= lastFireTime + gunData.timeBetFire)
+        { 
+            //마지막 총 발사 시점 갱신
+            lastFireTime = Time.time;
+            //실제 발사 처리 진행
+            Shot();
+        }
 
     }
 
     // 실제 발사 처리
     private void Shot() {
-      
+        RaycastHit hit;         // 레이캐스트에 의한 충돌 정보를 지정하는 컨테이너
+        Vector3 hitPosition = Vector3.zero; // 탄알이 맞은 곳을 저장할 변수
+        if(Physics.Raycast(fireTransform.position, fireTransform.forward, out hit, fireDistance))  // 레이캐스트(시작 지점, 방향, 충돌 정보 컨테이너, 사정거리)
+        {
+            // 레이가 어떤 물체와(콜라이더가 있는) 충돌한 경우
+
+            // 충돌한 상대방으로부터 IDamageable 오브젝트 가져오기 시도
+            IDamageable target = hit.collider.GetComponent<IDamageable>();
+
+            // 상대방으로부터 Idamageable 오브젝트 가져오는데 성공했다면
+            if (target != null ) 
+            {
+                // 상대방의 OnDamage 함수를 실행시켜 상대방에 데미지 주기
+                target.OnDamage(gunData.damage, hit.point, hit.normal);
+            }
+
+            // 레이가 충돌한 위치 저장
+            hitPosition = hit.point;
+        }
+
+        else
+        { 
+            // 레이가 다른 물체와 충돌하지 않았다면
+            // 탄알이 최대 사정거리까지 날아갔을 때의 위치를 충돌 위치로 사용
+            hitPosition = fireTransform.position + fireTransform.forward * fireDistance;
+        }
+
+        // 발사 이펙트 재생 시작
+        StartCoroutine(ShotEffect(hitPosition));
+
+        // 탄알 소비 처리 (남은 탄알 수 -1)
+        --magAmmo;
+        if( magAmmo > 0 )
+        {
+            // 탄창에 남은 탄알이 없는지 체크해서 상태를 Empty로 갱신
+            state = State.Ready;
+        }
     }
 
     // 발사 이펙트와 소리를 재생하고 탄알 궤적을 그림
